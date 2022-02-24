fcdc483526011ab0a065b50c02486796e05f4b8b
Collo <mutaic84@gmail.com>
2022-02-24 17:20:06 +0300

    add functionality to actually scroll up to top of page on clicking scroll up button

--- script.js
+++ script.js
@@ -3,9 +3,20 @@ $(document).ready(function(){
         if (this.scrollY > 20) {
             $('.navbar').addClass("sticky");
         } else {
-            $('.navbar').removeClass("sticky");
+            $('navbar').removeClass("sticky");
+        }
+        if (this.scrollY > 500) {
+            $('.scroll-up-btn').addClass("show");
+        } else {
+            $('.scroll-up-btn').removeClass("show");
         }
     });
+
+    // slide up script 
+    $('.scroll-up-btn').click(function(){
+        $('html').animate({scrollTop: 0});
+    });
+
     // navbar toggle 
     $('.menu-btn').click(function(){
         $('.navbar .menu').toggleClass("active");
